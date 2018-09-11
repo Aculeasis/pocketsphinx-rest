1d4fa4ec0f99a46baf7e4ed7cccf01fb1bf308bf
aculeasis <42580940+Aculeasis@users.noreply.github.com>
2018-09-11 21:31:18 +0300

    Update docker-starter

--- docker_starter.py
+++ docker_starter.py
@@ -6,7 +6,6 @@ import socket
 import subprocess
 import sys
 import threading
-import time
 import urllib.error
 import urllib.request
 import platform
@@ -81,7 +80,7 @@ def _docker_remote_sha256(rep_tag: str):
     return sha256
 
 
-def __docker_run_fatal(cmd: list, fatal: bool = False, stderr=subprocess.PIPE, stdout=subprocess.PIPE):
+def _docker_run_fatal(cmd: list, fatal: bool = False, stderr=subprocess.PIPE, stdout=subprocess.PIPE):
     run = subprocess.run(['docker', ] + cmd, stderr=stderr, stdout=stdout)
     if run.returncode:
         if fatal:
@@ -90,87 +89,73 @@ def __docker_run_fatal(cmd: list, fatal: bool = False, stderr=subprocess.PIPE, s
 
 
 def _docker_test() -> bool:
-    return not __docker_run_fatal(['ps', ]).returncode
-
-
-def _docker_containers() -> list:
-    # вернет список из [имя контейнера, ], ps -a --format "{{.Names}}"
-    run = __docker_run_fatal(['ps', '-a', '--format', '{{.Names}}'], True)
-    return [line for line in run.stdout.decode().split('\n') if len(line) > 2]
+    return not _docker_run_fatal(['ps', ]).returncode
 
 
 def _docker_stop(name: str) -> bool:
-    return not __docker_run_fatal(['stop', name]).returncode
+    return not _docker_run_fatal(['stop', name]).returncode
 
 
 def _docker_start(name: str) -> bool:
-    return not __docker_run_fatal(['start', name]).returncode
+    return not _docker_run_fatal(['start', name]).returncode
 
 
 def _docker_rm(name: str) -> bool:
-    return not __docker_run_fatal(['rm', name]).returncode
+    return not _docker_run_fatal(['rm', name]).returncode
 
 
 def _docker_rmi(rep_tag: str) -> bool:
-    return not __docker_run_fatal(['rmi', rep_tag]).returncode
+    return not _docker_run_fatal(['rmi', rep_tag]).returncode
 
 
 def _docker_pull(rep_tag: str) -> bool:
     # noinspection PyTypeChecker
-    return not __docker_run_fatal(cmd=['pull', rep_tag], stdout=None, stderr=None).returncode
+    return not _docker_run_fatal(cmd=['pull', rep_tag], stdout=None, stderr=None).returncode
 
 
 def _docker_build(rep_tag: str, file: str, path: str) -> bool:
     cmd = ['build', '--rm', '--no-cache', '-t', rep_tag, '-f', file, path]
     # noinspection PyTypeChecker
-    return not __docker_run_fatal(cmd=cmd, stdout=None, stderr=None).returncode
+    return not _docker_run_fatal(cmd=cmd, stdout=None, stderr=None).returncode
 
 
-def _docker_run(cmd: list):
+def _docker_run(cmd: list) -> bool:
     # noinspection PyTypeChecker
-    return not __docker_run_fatal(cmd=['run', ] + cmd, stderr=None).returncode
+    return not _docker_run_fatal(cmd=['run', ] + cmd, stderr=None).returncode
 
 
-def _docker_images_sha256() -> dict:
-    # docker images --digests --format "{{.Repository}}:{{.Tag}} {{.Digest}}"
-    run = __docker_run_fatal(['images', '--digests', '--format', '{{.Repository}}:{{.Tag}} {{.Digest}}'], True)
-    data = {}
-    for line in run.stdout.decode().strip('\n').rsplit('\n'):
-        if len(line) < 3:
-            continue
-        rep_tag, sha256 = line.rsplit(' ', 1)
-        data[rep_tag] = sha256
-    return data
-
-
-def _docker_images_id() -> dict:
-    # docker images --format {{.Repository}}:{{.Tag}} {{.ID}}
-    run = __docker_run_fatal(['images', '--format', '{{.Repository}}:{{.Tag}} {{.ID}}'], True)
-    data = {}
-    for line in run.stdout.decode().strip('\n').rsplit('\n'):
-        if len(line) < 3:
-            continue
-        rep_tag, id_ = line.rsplit(' ', 1)
-        data[rep_tag] = id_
-    return data
+def _docker_image_id_from_container(name):
+    run = _docker_run_fatal(['ps', '-a', '--format', '{{.Names}} {{.Image}}'], True)
+    for line in run.stdout.decode().strip('\n').split('\n'):
+        data = line.split(' ')
+        if len(data) == 2 and data[0] == name:
+            return data[1]
+    return None
 
 
 def _docker_repo_id() -> set:
-    # docker images --format {{.ID}}
-    run = __docker_run_fatal(['images', '--format', '{{.ID}}'], True)
+    run = _docker_run_fatal(['images', '--format', '{{.ID}}'], True)
     return {line for line in run.stdout.decode().strip('\n').rsplit('\n')}
 
 
 class DockerStarter:
-    def __init__(self, cfg: dict or list):
+    def __init__(self, cfg: dict or list, unit_name: str or None=None):
         self._cfg = cfg if type(cfg) is list else [cfg, ]
-        self._args = self._cli_parse()
+        self._args, install = self._cli_parse(self._allow_b())
         self._check()
-        self._containers = _docker_containers()
+        if install is not None:
+            SystemD(install, unit_name or self._cfg[0]['name'])
+            return
         if self._args.t:
-            self._all_once()
+            [run.join() for run in [_StarterWorker(cfg, self._args) for cfg in self._cfg]]
         else:
-            self._one_by_one()
+            [_StarterWorker(cfg, self._args).join() for cfg in self._cfg]
+
+    def _allow_b(self):
+        allow = False
+        for cfg in self._cfg:
+            allow |= os.path.isdir(cfg.get('docker_path', '')) and os.path.isfile(cfg.get('dockerfile', ''))
+        return allow
 
     def _check(self):
         if OS != 'linux':
@@ -191,25 +176,8 @@ class DockerStarter:
                 exit(1)
             images.add(cfg['image'])
 
-    def _all_once(self):
-        runs = [_StarterWorker(cfg, self._args, self._containers) for cfg in self._cfg]
-        work = True
-        while work:
-            work = False
-            time.sleep(2)
-            for run in runs:
-                if run.status() is None:
-                    work = True
-                    break
-
-    def _one_by_one(self):
-        for cfg in self._cfg:
-            run = _StarterWorker(cfg, self._args, self._containers)
-            while run.status() is None:
-                time.sleep(2)
-
     @staticmethod
-    def _cli_parse():
+    def _cli_parse(allow_b):
         def key_val(string):
             data = string.split('=', 1)
             if len(data) != 2:
@@ -227,43 +195,48 @@ class DockerStarter:
         one.add_argument('--restart', action='store_true', help='Run --stop && --start')
 
         parser.add_argument('-e', action='append', type=key_val, metavar='KEY=VAL', help='Add more env')
-        parser.add_argument('-b', action='store_true', help='Build images from Dockerfile, no pull from hub')
+        if allow_b and OS != 'windows':
+            parser.add_argument('-b', action='store_true', help='Build images from Dockerfile, no pull from hub')
         parser.add_argument('-t', action='store_true', help='Threaded works (Dangerous)')
         parser.add_argument('-f', action='store_true', help='Allow upgrade image from other source (hub or -b)')
-        return parser.parse_args()
+        if OS == 'linux':
+            two = parser.add_mutually_exclusive_group()
+            two.add_argument('--install', action='store_true', help='Install systemd unit')
+            one.add_argument('--uninstall', action='store_true', help='Remove systemd unit')
+        install = None
+        args = parser.parse_args()
+        if vars(args).get('uninstall'):
+            install = False
+        elif vars(args).get('install'):
+            install = True
+        return args, install
 
 
 class _StarterWorker(threading.Thread):
-    def __init__(self, cfg, cli, containers):
+    def __init__(self, cfg: dict, cli):
         super().__init__()
         self._cfg = cfg
         self._cli = cli
-        self._containers = containers
-        self._status = None
         self.start()
-
-    def status(self):
-        return self._status
 
     def run(self):
         if not self._config_check():
             pass
         elif self._cli.start:
-            self._start()
+            self._c_start()
         elif self._cli.stop:
-            self._stop()
+            self._c_stop()
         elif self._cli.update:
-            self._update()
+            self._c_update()
         elif self._cli.upgrade:
-            self._upgrade()
+            self._c_upgrade()
         elif self._cli.remove:
-            self._remove()
+            self._c_remove()
         elif self._cli.purge:
-            self._purge()
+            self._c_purge()
         elif self._cli.restart:
-            if self._stop():
-                self._start()
-        self._status = 0
+            if self._c_stop():
+                self._c_start()
 
     def _config_check(self):
         for key in ['name', 'image', 'data_path']:
@@ -271,7 +244,7 @@ class _StarterWorker(threading.Thread):
                 print('\'{}\' must be present'.format(key))
                 return False
         type_check = {
-            str: ['name', 'image', 'data_path', 'dockerfile', 'data_path', 'restart'],
+            str: ['name', 'image', 'data_path', 'dockerfile', 'docker_path', 'restart'],
             dict: ['p', 'v', 'e'],
             list: ['any', ]
         }
@@ -282,16 +255,16 @@ class _StarterWorker(threading.Thread):
                     return False
         return True
 
-    def _start(self):
-        if self._cfg['name'] in self._containers:
+    def _c_start(self):
+        if _docker_image_id_from_container(self._cfg['name']):
             result = '' if _docker_start(self._cfg['name']) else 'Failed '
             return print('{}start {}'.format(result, self._cfg['name']))
-        if self._cfg['image'] not in _docker_images_sha256() and not self._pull():
+        if self._get_image_data()['id'] is None and not self._pull():
             return print('Runtime error, exit.')
         self._run()
 
-    def _stop(self) -> bool:
-        if self._cfg['name'] in self._containers:
+    def _c_stop(self) -> bool:
+        if _docker_image_id_from_container(self._cfg['name']):
             if _docker_stop(self._cfg['name']):
                 print('stop {}'.format(self._cfg['name']))
             else:
@@ -301,79 +274,99 @@ class _StarterWorker(threading.Thread):
             print('Container {} not found.'.format(self._cfg['name']))
         return True
 
-    def _update(self, old_sha=None):
-        old_sha = old_sha or _docker_images_sha256().get(self._cfg['image'])
-        if old_sha is None:
+    def _c_update(self, old: dict or None = None):
+        old = old or self._get_image_data()
+        if old['sha256'] is None:
             if self._cli.update:
                 print('Local {} not found. Use --start'.format(self._cfg['image']))
             return True
         new_sha = _docker_remote_sha256(self._cfg['image'])
         if new_sha is None:
             return False
-        if old_sha == new_sha:
-            print('{} up to date'.format(self._cfg['image']))
+        got_name = self._cfg['image']
+        if old['name'] != got_name:
+            got_name = '{} ({})'.format(old['name'], got_name)
+        if old['sha256'] == new_sha:
+            print('{} up to date'.format(got_name))
             return False
         else:
             if self._cli.update:
-                msg = '. You build image? Use -b, -f for force pull' if old_sha == '<none>' else ''
-                print('{} update found. Use --upgrade{}'.format(self._cfg['image'], msg))
+                msg = '. You build image? Use -b, -f for force pull' if old['sha256'] == '<none>' else ''
+                print('{} update found. Use --upgrade{}'.format(got_name, msg))
             return True
 
-    def _upgrade(self):
-        old_sha = _docker_images_sha256().get(self._cfg['image'])
-        if old_sha is None:
-            return self._start()
-        old_id_ = _docker_images_id().get(self._cfg['image'])
-        if old_id_ is None:
-            return print('Runtime error,{} ID not found, exit'.format(self._cfg['name']))
-        if not self._allow_source_change(old_sha):
+    def _c_upgrade(self):
+        old = self._get_image_data()
+        if old['sha256'] is None:
+            return self._c_start()
+        if old['id'] is None:
+            return print('Runtime error,{} ID not found, exit'.format(self._cfg['image']))
+        if not self._allow_source_change(old['sha256']):
             return
-        if not (self._update(old_sha) or self._cli.b):
+        if not (self._c_update(old) or vars(self._cli).get('b', False)):
             return
         if not self._pull():
             return print('Runtime error, exit')
-        if self._stop() and self.__remove():
+        if self._c_stop() and self._rm():
             self._run()
-        name, _ = self._cfg['image'].rsplit(':', 1)
-        self._rmi(old_id_)
+        self._rmi(old)
 
     def _allow_source_change(self, old_sha):
         # Переключится с хаба на локальные сборки или обратно можно только с -f
         sources = ['hub', 'local build']
         old = sources[0] if old_sha != '<none>' else sources[1]
-        new = sources[0] if not self._cli.b else sources[1]
+        new = sources[0] if not vars(self._cli).get('b', False) else sources[1]
         if old != new and not self._cli.f:
             print('Disallow! Use -f for change image source from {} to {}'.format(old, new))
             return False
         return True
 
     def _pull(self):
-        if self._cli.b:
+        if vars(self._cli).get('b', False):
             return _docker_build(self._cfg['image'], self._cfg['dockerfile'], self._cfg['docker_path'])
         else:
             return _docker_pull(self._cfg['image'])
 
-    def _remove(self):
-        if self._stop() and self.__remove():
-            pass
-        return self._rmi()
+    def _c_remove(self):
+        data = self._get_image_data()
+        result = self._c_stop() and self._rm()
+        self._rmi(data)
+        return result
 
-    def __remove(self):
-        if self._cfg['name'] in self._containers:
+    def _rm(self):
+        if _docker_image_id_from_container(self._cfg['name']):
             return _docker_rm(self._cfg['name'])
         return True
 
-    def _rmi(self, id_=None) -> bool:
-        # IMAGE ID or registry:tag
-        if (id_ is not None and id_ in _docker_repo_id()) or (self._cfg['image'] in _docker_images_sha256()):
-            if not _docker_rmi(id_ or self._cfg['image']):
-                print('Error delete {} image. Maybe containers use it?'.format(id_ or self._cfg['image']))
+    @staticmethod
+    def _rmi(data: dict) -> bool:
+        if data['id'] in _docker_repo_id():
+            if not _docker_rmi(data['id']):
+                print('Error delete {name} ({id}) image. Maybe containers use it?'.format(**data))
                 return False
         return True
 
-    def _purge(self):
-        if self._remove():
+    def _c_purge(self):
+        if self._c_remove():
             shutil.rmtree(self._cfg['data_path'], ignore_errors=True)
+
+    def _get_image_data(self) -> dict:
+        id_ = _docker_image_id_from_container(self._cfg['name'])
+        run = _docker_run_fatal(
+            ['images', '--digests', '--format', '{{.Repository}}:{{.Tag}} {{.ID}} {{.Digest}}'],
+            True
+        )
+        if id_ == self._cfg['image']:
+            id_ = None
+        for line in run.stdout.decode().strip('\n').split('\n'):
+            data = line.split(' ')
+            if len(data) != 3:
+                continue
+            if id_ and id_ == data[1]:
+                return {'name': data[0], 'id': data[1], 'sha256': data[2]}
+            elif not id_ and data[0] == self._cfg['image']:
+                return {'name': data[0], 'id': data[1], 'sha256': data[2]}
+        return {'name': None, 'id': None, 'sha256': None}
 
     def _run(self):
         cmd = ['-d', ]
@@ -408,3 +401,92 @@ class _StarterWorker(threading.Thread):
         msg = 'docker run' if result else 'Failed docker run'
         print('{} {}'.format(msg, ' '.join(cmd)))
         return result
+
+
+class SystemD:
+    def __init__(self, action, name):
+        self._root_test()
+        name = '{} auto'.format(name)
+        f_name = '_'.join(name.lower().split())
+        self._files = ['{}.service'.format(f_name), '{}.timer'.format(f_name)]
+        self._systemd_path = '/etc/systemd/system/'
+        self._path = {
+            '_TIME_': '6h',
+            '_PARAMS_': self._get_params_str(),
+            '_MAIN_': os.path.abspath(sys.argv[0]),
+            '_NAME_': name
+        }
+        self._data = {k: self._getter(k) for k in self._files}
+        if action is None:
+            raise RuntimeError('Action is None')
+        elif action:
+            self.install()
+        else:
+            self.uninstall()
+
+    def install(self):
+        for k in self._data:
+            path = os.path.join(self._systemd_path, k)
+            with open(path, 'w') as fp:
+                fp.write(self._data[k])
+        self._systemd_reload()
+        self._systemd_enable()
+
+    def uninstall(self):
+        self._systemd_disable()
+        for k in self._data:
+            try:
+                os.remove(os.path.join(self._systemd_path, k))
+            except FileNotFoundError:
+                pass
+        self._systemd_reload()
+
+    @staticmethod
+    def _get_params_str() -> str:
+        params = sys.argv[1:]
+        for rm in ['--install', '--uninstall']:
+            if rm in params:
+                params.remove(rm)
+        return ' '.join(params)
+
+    @staticmethod
+    def _root_test():
+        if os.geteuid() != 0:
+            print('--(un)install need root privileges. Use sudo, bye.')
+            exit(1)
+
+    @staticmethod
+    def _systemd_reload():
+        subprocess.run(['systemctl', 'daemon-reload'])
+
+    def _systemd_enable(self):
+        subprocess.run(['systemctl', 'enable', self._files[1]])
+        subprocess.run(['systemctl', 'start', self._files[1]])
+
+    def _systemd_disable(self):
+        subprocess.run(['systemctl', 'stop', self._files[1]])
+        subprocess.run(['systemctl', 'disable', self._files[1]])
+
+    def _getter(self, file: str) -> str:
+        d = {
+            self._files[0]: [
+                '[Unit]',
+                'Description={_NAME_} job',
+                '',
+                '[Service]',
+                'Type=oneshot',
+                'ExecStart=/usr/bin/python3 -u {_MAIN_} {_PARAMS_}'
+            ],
+            self._files[1]: [
+                '[Unit]',
+                'Description={_NAME_}',
+                '',
+                '[Timer]',
+                'OnBootSec=15min',
+                'OnUnitActiveSec={_TIME_}',
+                '',
+                '[Install]',
+                'WantedBy=timers.target'
+            ]
+        }
+        return '\n'.join(d[file]).format(**self._path)
